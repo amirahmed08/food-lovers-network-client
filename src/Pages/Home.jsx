9eeee0d0f05230512f8ce3dc32f6343f2d92bc34
unknown <amirahmed7208@gmail.com>
2026-05-09 12:41:47 +0600

    code fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../Components/Header/header'
+import Header from '../Components/Header/Header'
 import LatestFoods from '../Components/LatestFoods/LatestFoods'
 import { useLoaderData } from 'react-router'
 
